e29516c5a22dbceb737d88f275e20bf4b0338fd9
walfredcutaran <cutaranwalfred1218@gmail.com>
2022-06-27 14:55:13 +0800

    v0.8.3
    *Added exception handling on pressing download w/out input
    
    *bugs
     - if slow internet (crashes)
    - no internet (crashes)

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from pytube import YouTube
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.image import AsyncImage
+from kivy.uix.progressbar import ProgressBar
 from pytube.exceptions import PytubeError, VideoUnavailable
-
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 
 class MyGridLayout(GridLayout):
-
     from android.permissions import request_permissions, Permission
     request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
@@ -25,55 +26,59 @@ class MyGridLayout(GridLayout):
     vid_owner = StringProperty('')
     vid_length = StringProperty('')
 
+    # vid_thumbnail = StringProperty('https://www.canva.com/design/DAFEfjiUSho/view')
+
     def __init__(self, **kwargs):
         # call grid layout constructor
         super(MyGridLayout, self).__init__(**kwargs)
+        self.padding = '10dp'
+        self.spacing = '10dp'
 
         self.cols = 1
-        self.add_widget(Label(text="pyTube", font_size=60, color=(209/255.0, 73/255.0, 73/255.0)))
 
         self.subgrid = GridLayout()
         self.subgrid.cols = 2
 
-        self.subgrid.add_widget(Label(text="Youtube Link: ", font_size=20))
+        self.subgrid.add_widget(Label(text="Youtube Link: ", font_size='20dp'))
 
         self.name = TextInput(multiline=False, text='')
         self.subgrid.add_widget(self.name)
         self.add_widget(self.subgrid)
 
         # submit button
-        self.submit = Button(text="Submit")
+        self.submit = Button(text="Submit", font_size='25dp')
         self.submit.bind(on_press=self.youtube_link)
         self.add_widget(self.submit)
 
-        self.title = Label(text=str(self.vid_title), font_size=20)
+        self.title = Label(text=str(self.vid_title), font_size='15dp')
         self.add_widget(self.title)
 
-        self.views = Label(text=str(self.vid_views), font_size=20)
+        self.views = Label(text=str(self.vid_views), font_size='15dp')
         self.add_widget(self.views)
 
-        self.owner = Label(text=str(self.vid_owner), font_size=20)
+        self.owner = Label(text=str(self.vid_owner), font_size='15dp')
         self.add_widget(self.owner)
 
-        self.length = Label(text=str(self.vid_length), font_size=20)
+        self.length = Label(text=str(self.vid_length), font_size='15dp')
         self.add_widget(self.length)
 
-        self.download = Button(text="Download")  # highest quality available only
+        self.download = Button(text="Download", font_size='25dp')  # highest quality available only
         self.download.bind(on_press=self.download_button_event)
+        self.add_widget(self.download)
 
     def youtube_link(self, instance):
 
         try:
-            name = self.name.text # name = yt link
+            # name = yt link
+            name = self.name.text
             video_object = YouTube(name)
-            # on_complete_callback=on_complete
 
         except PytubeError:
             err_popup_pe = Popup(title='Exception: Pytube Error',
                                  content=Label(text='Please input a valid Youtube link...'),
                                  auto_dismiss=True,
                                  size_hint=(None, None),
-                                 size=(600, 300))
+                                 size=('300dp', '100dp'))
             err_popup_pe.open()
 
         except VideoUnavailable:
@@ -81,15 +86,15 @@ class MyGridLayout(GridLayout):
                                  content=Label(text='The chosen video is unavailable...'),
                                  auto_dismiss=True,
                                  size_hint=(None, None),
-                                 size=(600, 300))
+                                 size=('300dp', '100dp'))
             err_popup_vu.open()
 
         else:
             instruction = Popup(title='Important Instruction',
-                                content=Label(text='Once you press download please wait for the next popup'),
+                                content=Label(text='Once you press download\nplease wait for the next popup...'),
                                 auto_dismiss=True,
                                 size_hint=(None, None),
-                                size=(600, 300))
+                                size=('300dp', '150dp'))
             instruction.open()
 
             title = video_object.title
@@ -101,7 +106,7 @@ class MyGridLayout(GridLayout):
             hours = mins / 60
 
             # length processing
-            final_length = f'{int(hours)} hour(s) {int(mins%60)} minute(s) {int(secs%60)} second(s)'
+            final_length = f'{int(hours)} hour(s) {int(mins % 60)} minute(s) {int(secs % 60)} second(s)'
 
             self.vid_title = title
             self.vid_views = str(f'{views:,}')  # comma separator
@@ -113,27 +118,42 @@ class MyGridLayout(GridLayout):
             self.owner.text = str(f'Owner: {self.vid_owner}')
             self.length.text = str(f'Length: {self.vid_length}')
 
-            self.add_widget(self.download)
-
     def download_button_event(self, instance):
-        from android.storage import primary_external_storage_path
-        dir = primary_external_storage_path()
-        download_dir_path = os.path.join(dir, 'Download')
 
-        link = self.name.text
-        video_object = YouTube(link, on_complete_callback=self.on_complete)
-        video_object.streams.get_highest_resolution().download(output_path=download_dir_path)
+        try:
+            name = self.name.text
+            video_object = YouTube(name)
 
-        # clear input box
-        self.name.text = ""
+        except PytubeError:
+            instruction = Popup(title='Exception: Missing Input',
+                                content=Label(text='Please input a valid youtube link\nbefore downloading...'),
+                                auto_dismiss=True,
+                                size_hint=(None, None),
+                                size=('300dp', '150dp'))
+            instruction.open()
+
+        else:
+            from android.storage import primary_external_storage_path
+            dir = primary_external_storage_path()
+            download_dir_path = os.path.join(dir, 'Download')
+
+            link = self.name.text
+            video_object = YouTube(link, on_complete_callback=self.on_complete)
+            video_object.streams.get_highest_resolution().download(output_path=download_dir_path)
 
     def on_complete(self, instance, file_path):
-        download_complete = Popup(title='Download Progress',
-                                  content=Label(text='Download Complete'),
+        download_complete = Popup(title='Download Complete',
+                                  content=Label(text='The file is on your\nlocal downloads folder...'),
                                   auto_dismiss=True,
                                   size_hint=(None, None),
-                                  size=(300, 200))
+                                  size=('300dp', '200dp'))
         download_complete.open()
+
+        self.name.text = ""
+        self.title.text = ""
+        self.views.text = ""
+        self.owner.text = ""
+        self.length.text = ""
 
 
 class MyApp(App):
